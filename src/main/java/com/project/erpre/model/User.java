a78d41403d3deee01b453a51240e088270fffc31
groovyplanet <sneakersgroovy@gmail.com>
2024-09-06 21:15:10 +0900

    0906 Han 주석 추가

--- src/main/java/com/project/erpre/model/User.java
+++ src/main/java/com/project/erpre/model/User.java
@@ -25,6 +25,7 @@ public class User {
     private String department;
 
     private LocalDateTime createdAt;
+//    여기를 M_EMPLOYEE 변경 후 >> HEADER.JS / LAYOUT.JS 참고해서 값 가져오면 됨 !
 
     @PrePersist
     protected void onCreate() {
