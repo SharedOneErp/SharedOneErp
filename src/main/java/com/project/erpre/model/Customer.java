c4f2d4f204001055a5cbabfb22381ac880ccdb47
Ihyeon <I2hyeonn@gmail.com>
2024-09-09 02:26:55 +0900

    0909 sim - Update JAP entities to match table schema

---
+++ src/main/java/com/project/erpre/model/Customer.java
@@ -0,0 +1,59 @@
+package com.project.erpre.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "m_customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_no")
+    private Long customerNo;
+
+    @Column(name = "customer_name", length = 100, nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_addr", length = 200)
+    private String customerAddr;
+
+    @Column(name = "customer_contact_no", length = 20, nullable = false)
+    private String customerContactNo;
+
+    @Column(name = "customer_business_reg_no", length = 20)
+    private String customerBusinessRegNo;
+
+    @Column(name = "customer_manager_name", length = 50, nullable = false)
+    private String customerManagerName;
+
+    @Column(name = "customer_manager_email", length = 100)
+    private String customerManagerEmail;
+
+    @Column(name = "customer_manager_contact_no", length = 20, nullable = false)
+    private String customerManagerContactNo;
+
+    @Column(name = "customer_insert_date", nullable = false)
+    private Timestamp customerInsertDate;
+
+    @Column(name = "customer_update_date")
+    private Timestamp customerUpdateDate;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderHead> orderHeads;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Price> prices;
+
+}
---
+++ src/main/java/com/project/erpre/model/OrderHead.java
@@ -0,0 +1,49 @@
+package com.project.erpre.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "m_order_h")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OrderHead {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_h_no")
+    private Long orderHNo;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_no", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "order_h_total_price", nullable = false, precision = 15, scale = 2)
+    private BigDecimal orderHTotalPrice;
+
+    @Column(name = "order_d_status", length = 10)
+    private String orderDStatus;
+
+    @Column(name = "order_d_insert_date", nullable = false)
+    private Timestamp orderDInsertDate;
+
+    @Column(name = "order_d_update_date")
+    private Timestamp orderDUpdateDate;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "orderHead", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderDetail> orderDetails;
+
+}
