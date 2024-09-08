e77fc027ef192b631f92c5d5cd3c9280e564b289
Ihyeon <I2hyeonn@gmail.com>
2024-09-09 03:02:50 +0900

    0909 sim - Update JPA entities

---
+++ src/main/java/com/project/erpre/model/OrderDetail.java
@@ -0,0 +1,43 @@
+package com.project.erpre.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "m_order_d")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_d_no")
+    private Long orderDNo;
+
+    @ManyToOne
+    @JoinColumn(name = "order_h_no", nullable = false)
+    private OrderHead orderHead;
+
+    @ManyToOne
+    @JoinColumn(name = "product_cd", nullable = false)
+    private Product product;
+
+    @Column(name = "order_d_price", nullable = false, precision = 15, scale = 2)
+    private BigDecimal orderDPrice;
+
+    @Column(name = "order_d_qty", nullable = false)
+    private int orderDQty;
+
+    @Column(name = "order_d_total_price", nullable = false, precision = 15, scale = 2)
+    private BigDecimal orderDTotalPrice;
+
+    @Column(name = "order_d_delivery_request_date")
+    private Timestamp orderDDeliveryRequestDate;
+}
