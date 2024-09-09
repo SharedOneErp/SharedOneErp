7f77fdb753f09750466fa5e0f80b99bf97990b2e
minseokSon <aksen1419@gmail.com>
2024-09-10 02:17:00 +0900

    0909 son workovertime

--- src/main/java/com/project/erpre/model/Employee.java
+++ src/main/java/com/project/erpre/model/Employee.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -23,26 +24,36 @@ public class Employee {
     @Column(name = "employee_pw", length = 50, nullable = false)
     private String employeePw;
 
-    @Column(name = "employee_name", length = 30, nullable = false)
+    @Column(name = "employee_name", length = 50, nullable = false) // 길이 수정
     private String employeeName;
 
     @Column(name = "employee_email", length = 30)
     private String employeeEmail;
 
-    @Column(name = "employee_tel", length = 20, nullable = false)
-    private String employeeTel;
+    @Column(name = "employee_contact", length = 20, nullable = false) // employee_tel -> employee_contact로 수정
+    private String employeeContact;
 
     @Column(name = "employee_role", length = 20, nullable = false)
     private String employeeRole;
 
-    @Column(name = "employee_insert_date", nullable = false)
+    @Column(name = "employee_insert_date", nullable = false, updatable = false)
     private Timestamp employeeInsertDate;
 
     @Column(name = "employee_update_date")
     private Timestamp employeeUpdateDate;
 
+    // 하나의 직원이 여러 개의 주문을 가질 수 있따
     @ToString.Exclude
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderHead> orderHeads;
 
+    @PrePersist
+    protected void onCreate() {
+        this.employeeInsertDate = Timestamp.valueOf(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.employeeUpdateDate = Timestamp.valueOf(LocalDateTime.now());
+    }
 }
