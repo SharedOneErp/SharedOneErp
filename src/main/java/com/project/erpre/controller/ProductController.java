9b345f5c9cf49e113544addc44f03819c129f092
Ihyeon <I2hyeonn@gmail.com>
2024-09-10 20:48:27 +0900

    0910 sim - update prodectDetail

--- src/main/java/com/project/erpre/controller/ProductController.java
+++ src/main/java/com/project/erpre/controller/ProductController.java
@@ -24,19 +24,16 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    // 상품 상세 조회 API
+    @GetMapping("/productDetail/{productCd}")
+    public List<ProductDTO> getProductDetailsByProductCd(@PathVariable String productCd) {
+        return productService.getProductDetailsByProductCd(productCd);
+    }
+
     // 선택한 상품 삭제 API
     @DeleteMapping("/productDelete")
     public List<Product> deleteProducts(@RequestBody List<String> productCds) {
         return productService.deleteProducts(productCds);
     }
 
-    // 검색 API
-    @GetMapping("/search")
-    public List<Product> searchProducts(
-            @RequestParam(required = false) String productCd,
-            @RequestParam(required = false) Category category,
-            @RequestParam(required = false) String productNm
-    ) {
-        return productService.searchProducts(productCd, category, productNm);
-    }
 }
