e4380ba5d761d9ec7c9116a5601104eccfea0120
MinSeokSon <aksen1419@gmail.com>
2024-09-11 14:10:08 +0900

    0911 son worktime1

--- src/main/java/com/project/erpre/service/EmployeeService.java
+++ src/main/java/com/project/erpre/service/EmployeeService.java
@@ -3,6 +3,9 @@ package com.project.erpre.service;
 import com.project.erpre.model.Employee;
 import com.project.erpre.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,4 +20,9 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+//    public Page<Employee> getPageEmployees(int page, int size) {
+//        Pageable pageable = PageRequest.of(page, size);
+//        return employeeRepository.findAll(pageable);
+//    }
 }
