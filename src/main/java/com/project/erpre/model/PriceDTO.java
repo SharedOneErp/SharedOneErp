3c575065330ec666e742201e81de85f871325cba
Yewon Cho <qjqmti0113@gmail.com>
2024-09-18 04:20:43 +0900

    0918 cho update

--- src/main/java/com/project/erpre/model/PriceDTO.java
+++ src/main/java/com/project/erpre/model/PriceDTO.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.Column;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.sql.Date;
 
 /**
  * PriceDTO는 클라이언트와 서버 간의 데이터 전송을 위한 객체입니다.
@@ -22,8 +23,8 @@ public class PriceDTO {
     private Integer customerNo;  // 고객 번호 (m_customer 테이블의 고객 번호)
     private String productCd;  // 제품 코드 (m_product 테이블의 제품 코드)
     private BigDecimal priceCustomer; // 고객별 가격
-    private Timestamp priceStartDate; // 가격 적용 시작 일자
-    private Timestamp priceEndDate; // 가격 적용 종료 일자
+    private Date priceStartDate; // 가격 적용 시작 일자
+    private Date priceEndDate; // 가격 적용 종료 일자
     private Timestamp priceInsertDate; // 가격 등록 일시
     private Timestamp priceUpdateDate; // 가격 수정 일시
     private String priceDeleteYn = "N"; // 삭제 여부 기본값 'N'
