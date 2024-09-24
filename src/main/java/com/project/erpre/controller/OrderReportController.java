b3026be4eb3396930cfc3cf9995e81c6400dfcd3
minseokSon <aksen1419@gmail.com>
2024-09-24 22:59:55 +0900

    0924 son 건수합이 아닌 주문금액합계로 바꿈

--- src/main/java/com/project/erpre/controller/OrderReportController.java
+++ src/main/java/com/project/erpre/controller/OrderReportController.java
@@ -20,19 +20,19 @@ public class OrderReportController {
     private OrderReportService orderReportService;
 
     // 총 주문건수(기간동안의 총 주문건수를 집계하는 거, 일단은 안씀)
-    @GetMapping("/totalOrders")
-    public Long getTotalOrders(@RequestParam String startDate, @RequestParam String endDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); //요청이 String으로 오니까 변환해줘야함
-
-        // LocalDate로 변환 후, LocalDateTime으로 시작/끝 시간 추가
-        LocalDate start = LocalDate.parse(startDate, formatter);
-        LocalDate end = LocalDate.parse(endDate, formatter);
-
-        LocalDateTime startDateTime = start.atStartOfDay(); // 00:00:00 추가(localdatetime)
-        LocalDateTime endDateTime = end.atTime(23, 59, 59);  // 23:59:59 추가
-
-        return orderReportService.getTotalOrders(startDateTime, endDateTime);
-    }
+//    @GetMapping("/totalOrders")
+//    public Long getTotalOrders(@RequestParam String startDate, @RequestParam String endDate) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); //요청이 String으로 오니까 변환해줘야함
+//
+//        // LocalDate로 변환 후, LocalDateTime으로 시작/끝 시간 추가
+//        LocalDate start = LocalDate.parse(startDate, formatter);
+//        LocalDate end = LocalDate.parse(endDate, formatter);
+//
+//        LocalDateTime startDateTime = start.atStartOfDay(); // 00:00:00 추가(localdatetime)
+//        LocalDateTime endDateTime = end.atTime(23, 59, 59);  // 23:59:59 추가
+//
+//        return orderReportService.getTotalOrders(startDateTime, endDateTime);
+//    }
 
     // 달별 주문건수
     @GetMapping("/monthlyOrders")
@@ -77,45 +77,5 @@ public class OrderReportController {
         return orderReportService.getOrdersByYear(startDateTime, endDateTime);
     }    
 
-//    // 고객별 주문건수
-//    @GetMapping("/api/orderReport/ordersByCustomer")
-//    public List<Object[]> getOrdersByCustomer(@RequestParam String startDate, @RequestParam String endDate) {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//
-//        LocalDate start = LocalDate.parse(startDate, formatter);
-//        LocalDate end = LocalDate.parse(endDate, formatter);
-//
-//        LocalDateTime startDateTime = start.atStartOfDay();
-//        LocalDateTime endDateTime = end.atTime(23, 59, 59);
-//
-//        return orderReportService.getOrdersByCustomer(startDateTime, endDateTime);
-//    }
-//
-//    // 담당자별 주문건수
-//    @GetMapping("/api/orderReport/ordersByEmployee")
-//    public List<Object[]> getOrdersByEmployee(@RequestParam String startDate, @RequestParam String endDate) {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//
-//        LocalDate start = LocalDate.parse(startDate, formatter);
-//        LocalDate end = LocalDate.parse(endDate, formatter);
-//
-//        LocalDateTime startDateTime = start.atStartOfDay();
-//        LocalDateTime endDateTime = end.atTime(23, 59, 59);
-//
-//        return orderReportService.getOrdersByEmployee(startDateTime, endDateTime);
-//    }
-//
-//    // 상품별 주문건수
-//    @GetMapping("/api/orderReport/ordersByProduct")
-//    public List<Object[]> getOrdersByProduct(@RequestParam String startDate, @RequestParam String endDate) {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//
-//        LocalDate start = LocalDate.parse(startDate, formatter);
-//        LocalDate end = LocalDate.parse(endDate, formatter);
-//
-//        LocalDateTime startDateTime = start.atStartOfDay();
-//        LocalDateTime endDateTime = end.atTime(23, 59, 59);
-//
-//        return orderReportService.getOrdersByProduct(startDateTime, endDateTime);
-//    }
+
 }
--- src/main/java/com/project/erpre/service/OrderReportService.java
+++ src/main/java/com/project/erpre/service/OrderReportService.java
@@ -40,23 +40,10 @@ public class OrderReportService {
         return orderReportRepository.countOrdersByYear(startDate, endDate);
     }
             
-    // 총 주문건수
-    public Long getTotalOrders(LocalDateTime startDate, LocalDateTime endDate) {
-        return orderReportRepository.countTotalOrders(startDate, endDate);
-    }
+//    // 총 주문건수
+//    public Long getTotalOrders(LocalDateTime startDate, LocalDateTime endDate) {
+//        return orderReportRepository.countTotalOrders(startDate, endDate);
+//    }
 
-//    // 고객별 주문건수
-//    public List<Object[]> getOrdersByCustomer(LocalDateTime startDate, LocalDateTime endDate) {
-//        return orderReportRepository.countOrdersByCustomer(startDate, endDate);
-//    }
-//
-//    // 담당자별 주문건수
-//    public List<Object[]> getOrdersByEmployee(LocalDateTime startDate, LocalDateTime endDate) {
-//        return orderReportRepository.countOrdersByEmployee(startDate, endDate);
-//    }
-//
-//    // 상품별 주문건수
-//    public List<Object[]> getOrdersByProduct(LocalDateTime startDate, LocalDateTime endDate) {
-//        return orderReportRepository.countOrdersByProduct(startDate, endDate);
-//    }
+
 }
