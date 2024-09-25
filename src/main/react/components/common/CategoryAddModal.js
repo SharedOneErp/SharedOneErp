66f253bd7702d27b18066f12145415fde4faf220
hojin <hojinkim971597@gmail.com>
2024-09-25 21:16:29 +0900

    0925 kim - 카테고리 수정

--- src/main/react/components/common/CategoryAddModal.js
+++ src/main/react/components/common/CategoryAddModal.js
@@ -23,6 +23,16 @@ function CategoryModal({
   closeModal,
   handleBackgroundClick,
   isSubmitting,
+  setAllCategories,
+  setTopCategories,
+  setMidCategories,
+  setLowCategories,
+  allCategories,
+  topCategories,
+  midCategories,
+  lowCategories,
+  setSelectedCategory,
+  selectedCategory,
 }) {
 
 
@@ -38,20 +48,20 @@ function CategoryModal({
           {/* 대분류 */}
           <div className='level_wrap'>
             <h4>대분류
-              {getTopCategory.length > 0 && (
-                <span className="list_cnt">({getTopCategory.length})</span>
+              {topCategories.length > 0 && (
+                <span className="list_cnt">({topCategories.length})</span>
               )}
             </h4>
             <div className='content_wrap'>
               <div className='list_wrap'>
                 <ul className='list'>
-                  {getTopCategory.map((category) => (
+                  {topCategories.map((category) => (
                     <li key={category.categoryNo}
                       onClick={() => {
                         handleTopClick(category.categoryNo);
                         handleTopHover(category.categoryNo);
                       }}
-                      className={selectedTopCategory === category.categoryNo ? 'selected' : ''}
+                      className={selectedCategory.top === category.categoryNo ? 'selected' : ''}
                     >
                       <span className='category-span'>{category.categoryNm}</span>
                       <i className="bi bi-chevron-right"></i>
@@ -81,20 +91,20 @@ function CategoryModal({
           {/* 중분류 */}
           <div className='level_wrap'>
             <h4>중분류
-              {getMidCategory.length > 0 && (
-                <span className="list_cnt">({getMidCategory.length})</span>
+              {midCategories.length > 0 && (
+                <span className="list_cnt">({midCategories.length})</span>
               )}
             </h4>
             <div className='content_wrap'>
               <div className='list_wrap' style={{ position: 'relative' }}>
-                {getMidCategory.length === 0 ? (
-                  <p className='empty_wrap'><i className="bi bi-exclamation-circle"></i>데이터가 없습니다.</p>
+                {midCategories.length === 0 ? (
+                  <p className='empty_wrap'><i className="bi bi-exclamation-circle"></i>대분류를 선택해주세요.</p>
                 ) : (
                   <ul className='list'>
-                    {getMidCategory.map((category) => (
+                    {midCategories.map((category) => (
                       <li key={category.categoryNo}
                         onClick={() => handleMidClick(category.categoryNo)}
-                        className={selectedMidCategory === category.categoryNo ? 'selected' : ''}
+                        className={selectedCategory.middle === category.categoryNo ? 'selected' : ''}
                       >
                         <span className='category-span'>{category.categoryNm}</span>
                         <i className="bi bi-chevron-right"></i>
@@ -125,22 +135,22 @@ function CategoryModal({
           {/* 소분류 */}
           <div className='level_wrap'>
             <h4>소분류
-              {getLowCategory.length > 0 && (
-                <span className="list_cnt">({getLowCategory.length})</span>
+              {lowCategories.length > 0 && (
+                <span className="list_cnt">({lowCategories.length})</span>
               )}
             </h4>
             <div className='content_wrap'>
               <div className='list_wrap' style={{ position: 'relative' }}>
-                {!selectedMidCategory ? (
+                {!selectedCategory.middle ? (
                   <p className='empty_wrap'><i className="bi bi-exclamation-circle"></i>중분류를 선택해주세요.</p>
-                ) : getLowCategory.length === 0 ? (
+                ) : lowCategories.length === 0 ? (
                   <p className='empty_wrap'><i className="bi bi-exclamation-circle"></i>데이터가 없습니다.</p>
                 ) : (
                   <ul className='list'>
-                    {getLowCategory.map((category) => (
+                    {lowCategories.map((category) => (
                       <li key={category.categoryNo}
                         onClick={() => handleLowClick(category.categoryNo)}
-                        className={selectedLowCategory === category.categoryNo ? 'selected' : ''}
+                        className={selectedCategory.low === category.categoryNo ? 'selected' : ''}
                       >
                         <span className='category-span'>{category.categoryNm}</span>
                       </li>
@@ -149,7 +159,7 @@ function CategoryModal({
                 )}
               </div>
               {/* 소분류 input은 중분류가 선택된 경우에만 렌더링 */}
-              {selectedMidCategory && (
+              {selectedCategory.middle && (
                 <div className='input-wrap'>
                   <div className={`search_box ${insertLow ? 'has_text' : ''}`}>
                     <label className="label_floating">Enter키로 소분류 추가</label>
@@ -175,12 +185,12 @@ function CategoryModal({
         <div className='btn_wrap'>
           <div className='selected-cate'>
             <label className='floating-label'>선택된 카테고리</label>
-            {selectedLowCategory ? (
-              <span className='input-field' >{getLowCategory.find(category => category.categoryNo === selectedLowCategory)?.categoryNm || '선택된 카테고리 없음'}</span>
-            ) : selectedMidCategory ? (
-              <span className='input-field'>{getMidCategory.find(category => category.categoryNo === selectedMidCategory)?.categoryNm || '선택된 카테고리 없음'}</span>
-            ) : selectedTopCategory ? (
-              <span className='input-field'>{getTopCategory.find(category => category.categoryNo === selectedTopCategory)?.categoryNm || '선택된 카테고리 없음'}</span>
+            {selectedCategory.low ? (
+              <span className='input-field' >{lowCategories.find(category => category.categoryNo === selectedCategory.low)?.categoryNm || '선택된 카테고리 없음'}</span>
+            ) : selectedCategory.middle ? (
+              <span className='input-field'>{midCategories.find(category => category.categoryNo === selectedCategory.middle)?.categoryNm || '선택된 카테고리 없음'}</span>
+            ) : selectedCategory.top ? (
+              <span className='input-field'>{topCategories.find(category => category.categoryNo === selectedCategory.top)?.categoryNm || '선택된 카테고리 없음'}</span>
             ) : (
               <span>선택된 카테고리가 없습니다</span>
             )}
